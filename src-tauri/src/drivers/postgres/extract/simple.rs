8c209ec8b51734c52b7222b09b3b44a0a1f7f268
Marwan Hilal <marwan.hilal.dev+1@gmail.com>
2026-04-02 20:29:41 +0200

    refactor to add advanced types

--- src-tauri/src/drivers/postgres/extract/simple.rs
+++ src-tauri/src/drivers/postgres/extract/simple.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, net::IpAddr};
+use std::collections::HashMap;
 
 use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
 use rust_decimal::Decimal;
@@ -7,6 +7,8 @@ use tokio_postgres::types::{FromSql, Type};
 use uuid::Uuid;
 
 use crate::drivers::common::encode_blob;
+
+use super::advanced_types;
 
 #[inline]
 pub fn extract_or_null(ty: &Type, buf: &[u8]) -> JsonValue {
@@ -56,6 +58,8 @@ pub fn extract_or_null(ty: &Type, buf: &[u8]) -> JsonValue {
             from_sql_or_none::<DateTime<Utc>>(ty, buf)
                 .map(|dt| dt.format("%Y-%m-%d %H:%M:%S").to_string()),
         ),
+        Type::INTERVAL => JsonValue::from(from_sql_or_none::<advanced_types::Interval>(ty, buf)),
+        Type::TIMETZ => JsonValue::from(from_sql_or_none::<advanced_types::TimeTz>(ty, buf)),
 
         // json
         Type::JSON => JsonValue::from(from_sql_or_none::<JsonValue>(ty, buf)),
@@ -67,8 +71,91 @@ pub fn extract_or_null(ty: &Type, buf: &[u8]) -> JsonValue {
                 .unwrap_or_default()
         }
 
-        // ip address
-        Type::INET => JsonValue::from(from_sql_or_none::<IpAddr>(ty, buf).map(|ip| ip.to_string())),
+        // Network types
+        Type::MACADDR => JsonValue::from(from_sql_or_none::<advanced_types::MacAddr>(ty, buf)),
+        Type::MACADDR8 => JsonValue::from(from_sql_or_none::<advanced_types::MacAddr8>(ty, buf)),
+        Type::INET => JsonValue::from(from_sql_or_none::<advanced_types::CidrOrInet>(ty, buf)),
+        Type::CIDR => JsonValue::from(from_sql_or_none::<advanced_types::CidrOrInet>(ty, buf)),
+        Type::BIT => JsonValue::from(from_sql_or_none::<advanced_types::BitOrVarBit>(ty, buf)),
+        Type::VARBIT => JsonValue::from(from_sql_or_none::<advanced_types::BitOrVarBit>(ty, buf)),
+
+        // System Identifiers
+        Type::XID => JsonValue::from(from_sql_or_none::<advanced_types::Xid>(ty, buf)),
+        Type::CID => JsonValue::from(from_sql_or_none::<advanced_types::Cid>(ty, buf)),
+        Type::TID => JsonValue::from(from_sql_or_none::<advanced_types::Tid>(ty, buf)),
+        Type::XID8 => JsonValue::from(from_sql_or_none::<advanced_types::Xid8>(ty, buf)),
+
+        // Object References (The "Reg" Types)
+        Type::REGPROC => JsonValue::from(from_sql_or_none::<advanced_types::RegProc>(ty, buf)),
+        Type::REGPROCEDURE => {
+            JsonValue::from(from_sql_or_none::<advanced_types::RegProcedure>(ty, buf))
+        }
+        Type::REGOPER => JsonValue::from(from_sql_or_none::<advanced_types::RegOper>(ty, buf)),
+        Type::REGOPERATOR => {
+            JsonValue::from(from_sql_or_none::<advanced_types::RegOperator>(ty, buf))
+        }
+        Type::REGCLASS => JsonValue::from(from_sql_or_none::<advanced_types::RegClass>(ty, buf)),
+        Type::REGTYPE => JsonValue::from(from_sql_or_none::<advanced_types::RegType>(ty, buf)),
+        Type::REGCONFIG => JsonValue::from(from_sql_or_none::<advanced_types::RegConfig>(ty, buf)),
+        Type::REGDICTIONARY => {
+            JsonValue::from(from_sql_or_none::<advanced_types::RegDictionary>(ty, buf))
+        }
+        Type::REGNAMESPACE => {
+            JsonValue::from(from_sql_or_none::<advanced_types::RegNamespace>(ty, buf))
+        }
+        Type::REGROLE => JsonValue::from(from_sql_or_none::<advanced_types::RegRole>(ty, buf)),
+        Type::REGCOLLATION => {
+            JsonValue::from(from_sql_or_none::<advanced_types::RegCollation>(ty, buf))
+        }
+
+        // Geometric Types
+        Type::POINT => JsonValue::from(from_sql_or_none::<advanced_types::Point>(ty, buf)),
+        Type::LSEG => JsonValue::from(from_sql_or_none::<advanced_types::Lseg>(ty, buf)),
+        Type::BOX => JsonValue::from(from_sql_or_none::<advanced_types::PgBox>(ty, buf)),
+        Type::POLYGON => JsonValue::from(from_sql_or_none::<advanced_types::Polygon>(ty, buf)),
+        Type::PATH => JsonValue::from(from_sql_or_none::<advanced_types::Path>(ty, buf)),
+        Type::LINE => JsonValue::from(from_sql_or_none::<advanced_types::Line>(ty, buf)),
+        Type::CIRCLE => JsonValue::from(from_sql_or_none::<advanced_types::Circle>(ty, buf)),
+
+        // uft8 text
+        Type::JSONPATH => JsonValue::from(from_sql_or_none::<advanced_types::JsonPath>(ty, buf)),
+        Type::XML => JsonValue::from(from_sql_or_none::<advanced_types::Xml>(ty, buf)),
+        Type::REFCURSOR => JsonValue::from(from_sql_or_none::<advanced_types::RefCursor>(ty, buf)),
+        Type::ACLITEM => JsonValue::from(from_sql_or_none::<advanced_types::AclItem>(ty, buf)),
+        Type::PG_NODE_TREE => {
+            JsonValue::from(from_sql_or_none::<advanced_types::PgNodeTree>(ty, buf))
+        }
+
+        Type::MONEY => JsonValue::from(from_sql_or_none::<advanced_types::Money>(ty, buf)),
+
+        // Full Text Search types
+        Type::TS_VECTOR => JsonValue::from(from_sql_or_none::<advanced_types::TsVector>(ty, buf)),
+        Type::TSQUERY => JsonValue::from(from_sql_or_none::<advanced_types::TsQuery>(ty, buf)),
+        Type::GTS_VECTOR => JsonValue::from(from_sql_or_none::<advanced_types::GtsVector>(ty, buf)),
+
+        // Internal System, Snapshots & Statistics postgres types
+        Type::PG_LSN => JsonValue::from(from_sql_or_none::<advanced_types::PgLsn>(ty, buf)),
+        Type::PG_SNAPSHOT => {
+            JsonValue::from(from_sql_or_none::<advanced_types::TxidSnapshotOrPgSnapshot>(ty, buf))
+        }
+        Type::TXID_SNAPSHOT => {
+            JsonValue::from(from_sql_or_none::<advanced_types::TxidSnapshotOrPgSnapshot>(ty, buf))
+        }
+        Type::PG_NDISTINCT => {
+            JsonValue::from(from_sql_or_none::<advanced_types::PgNdistinct>(ty, buf))
+        }
+        Type::PG_DEPENDENCIES => {
+            JsonValue::from(from_sql_or_none::<advanced_types::PgDependencies>(ty, buf))
+        }
+        Type::PG_BRIN_BLOOM_SUMMARY => JsonValue::from(from_sql_or_none::<
+            advanced_types::PgBrinBloomSummary,
+        >(ty, buf)),
+        Type::PG_BRIN_MINMAX_MULTI_SUMMARY => {
+            JsonValue::from(from_sql_or_none::<advanced_types::PgBrinMinmaxMultiSummary>(ty, buf))
+        }
+        Type::PG_MCV_LIST => {
+            JsonValue::from(from_sql_or_none::<advanced_types::PgMcvList>(ty, buf))
+        }
 
         _ => JsonValue::Null,
     }
