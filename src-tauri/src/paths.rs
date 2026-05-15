ccb74e3aa37c32409efb87c7ce4f7f9270963b36
starasenko <starasenko-ext@goodgamestudios.com>
2026-05-15 23:06:12 +0400

    fix: incorrect get_app_config_dir when running in mpc mode in windows

--- src-tauri/src/paths.rs
+++ src-tauri/src/paths.rs
@@ -3,7 +3,15 @@ use std::path::PathBuf;
 
 pub fn get_app_config_dir() -> PathBuf {
     if let Some(proj_dirs) = ProjectDirs::from("", "", "tabularis") {
-        proj_dirs.config_dir().to_path_buf()
+
+        #[cfg(target_os = "windows")]
+        {
+            proj_dirs.config_dir().parent().unwrap().to_path_buf()
+        }
+        #[cfg(not(target_os = "windows"))]
+        {
+            proj_dirs.config_dir().to_path_buf()
+        }
     } else {
         // Fallback for weird environments
         PathBuf::from(".config/tabularis")
